c0b17b70ede5977639bd9ef5b5522fc5358328c2
SubjectFear <reklawmada67@yahoo.com>
2026-07-22 20:25:26 -0500

    WIP ContactSection.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -63,6 +63,7 @@ import FeaturesSection from "./components/FeaturesSection";
 import ProductShowcase from "./components/ProductShowcase";
 import CtaSection from "./components/CtaSection";
 import AboutSection from "./components/AboutSection";
+import ContactSection from "./components/ContactSection";
 
 export default function App() {
     return(
@@ -103,7 +104,7 @@ export default function App() {
 
             {/* CONTACT */}
             <section className="bg-cta" id="contact">
-
+            <ContactSection />
             </section>
 
             {/* FOOTER */}
